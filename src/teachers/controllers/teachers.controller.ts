0d3cddd004b509cc6d90fcfe1520bc6a19c2ebb3
Carolina Moutella <ola@carolinamoutella.com>
2024-06-19 19:25:35 -0300

    Zod: add validation

--- src/teachers/controllers/teachers.controller.ts
+++ src/teachers/controllers/teachers.controller.ts
@@ -6,17 +6,33 @@ import {
   Param,
   Post,
   Put,
+  UsePipes,
 } from '@nestjs/common';
 import { TeachersService } from '../service/teachers.service';
-import { InterfaceTeacher } from '../schemas/models/teacher.interface';
+import { z } from 'zod';
+import { ZodValidationPipe } from 'src/shared/pipe/zod-validation.pipe';
+
+const createTeacherSchema = z.object({
+  name: z.string(),
+  age: z.coerce.number(),
+});
+type CreateTeacher = z.infer<typeof createTeacherSchema>;
+
+const updateTeacherSchema = z.object({
+  name: z.string().optional(),
+  cpf: z.coerce.string().optional(),
+  age: z.number().optional(),
+});
+type UpdateTeacher = z.infer<typeof updateTeacherSchema>;
 
 @Controller('teacher')
 export class TeachersController {
   constructor(private readonly teachersService: TeachersService) {}
 
+  @UsePipes(new ZodValidationPipe(createTeacherSchema))
   @Post()
-  async createTeacher(@Body() teacher: InterfaceTeacher) {
-    await this.teachersService.createTeacher(teacher);
+  async createTeacher(@Body() { name, age }: CreateTeacher) {
+    await this.teachersService.createTeacher({ name, age });
   }
 
   @Get()
@@ -32,7 +48,7 @@ export class TeachersController {
   @Put(':id')
   async updateTeacher(
     @Param('id') id: string,
-    @Body() data: Partial<InterfaceTeacher>,
+    @Body(new ZodValidationPipe(updateTeacherSchema)) data: UpdateTeacher,
   ) {
     await this.teachersService.updateTeacher(id, data);
   }
